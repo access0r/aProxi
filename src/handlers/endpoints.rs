3a8f43387d38d6f64b7e49db13549fb282d46b86
accessor.eth <38918838+access0r@users.noreply.github.com>
2023-05-20 18:58:29 -0700

    Update endpoints.rs

--- src/handlers/endpoints.rs
+++ src/handlers/endpoints.rs
@@ -1,26 +1,22 @@
-use actix_web::{web, HttpResponse};
+use actix_web::{web, HttpResponse, Responder};
 
-pub async fn endpoint1() -> HttpResponse {
-    // Implementation for endpoint 1
-    HttpResponse::Ok().body("Endpoint 1")
+pub async fn endpoint1() -> impl Responder {
+    match perform_action() {
+        Ok(result) => HttpResponse::Ok().body(result),
+        Err(err) => HttpResponse::InternalServerError().body(format!("Error: {}", err)),
+    }
 }
 
-pub async fn endpoint2() -> HttpResponse {
-    // Implementation for endpoint 2
-    HttpResponse::Ok().body("Endpoint 2")
+pub async fn endpoint2() -> impl Responder {
+    match perform_action() {
+        Ok(result) => HttpResponse::Ok().body(result),
+        Err(err) => HttpResponse::InternalServerError().body(format!("Error: {}", err)),
+    }
 }
 
-pub async fn endpoint3() -> HttpResponse {
-    // Implementation for endpoint 3
-    HttpResponse::Ok().body("Endpoint 3")
-}
-
-pub async fn endpoint4() -> HttpResponse {
-    // Implementation for endpoint 4
-    HttpResponse::Ok().body("Endpoint 4")
-}
-
-pub async fn endpoint5() -> HttpResponse {
-    // Implementation for endpoint 5
-    HttpResponse::Ok().body("Endpoint 5")
+pub async fn endpoint3() -> impl Responder {
+    match perform_action() {
+        Ok(result) => HttpResponse::Ok().body(result),
+        Err(err) => HttpResponse::InternalServerError().body(format!("Error: {}", err)),
+    }
 }
