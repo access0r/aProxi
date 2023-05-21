0497e72a6150d78fcc84a1cf9b2387a1c33ff27f
accessor.eth <38918838+access0r@users.noreply.github.com>
2023-05-20 19:08:18 -0700

    Update endpoints.rs

--- src/handlers/endpoints.rs
+++ src/handlers/endpoints.rs
@@ -1,22 +1,44 @@
-use actix_web::{web, HttpResponse, Responder};
+use actix_web::{web, HttpResponse};
+use web3::types::Address;
 
-pub async fn endpoint1() -> impl Responder {
-    match perform_action() {
-        Ok(result) => HttpResponse::Ok().body(result),
-        Err(err) => HttpResponse::InternalServerError().body(format!("Error: {}", err)),
+pub async fn endpoint1() -> HttpResponse {
+    // Implementation for endpoint 1
+    HttpResponse::Ok().body("Endpoint 1")
+}
+
+pub async fn endpoint2() -> HttpResponse {
+    // Implementation for endpoint 2
+    HttpResponse::Ok().body("Endpoint 2")
+}
+
+pub async fn resolve_ens_name(
+    web::Path(ens_name): web::Path<String>,
+    web3: web::Data<Web3<Http>>,
+    contract_data: web::Data<models::ContractData>,
+) -> HttpResponse {
+    match resolve_ens(&web3, &contract_data, &ens_name).await {
+        Ok(resolved_address) => {
+            HttpResponse::Ok().body(format!("Resolved ENS name '{}': {:?}", ens_name, resolved_address))
+        }
+        Err(err) => HttpResponse::InternalServerError().body(format!("Error resolving ENS name: {}", err)),
     }
 }
 
-pub async fn endpoint2() -> impl Responder {
-    match perform_action() {
-        Ok(result) => HttpResponse::Ok().body(result),
-        Err(err) => HttpResponse::InternalServerError().body(format!("Error: {}", err)),
-    }
+pub async fn relay_endpoint_request(
+    web::Path(ens_address): web::Path<Address>,
+    web3: web::Data<Web3<Http>>,
+    contract_data: web::Data<models::ContractData>,
+) -> HttpResponse {
+    // Implementation for relaying endpoint request using contract data and Web3 instance
+    HttpResponse::Ok().body(format!("Relaying endpoint request for ENS address: {}", ens_address))
 }
 
-pub async fn endpoint3() -> impl Responder {
-    match perform_action() {
-        Ok(result) => HttpResponse::Ok().body(result),
-        Err(err) => HttpResponse::InternalServerError().body(format!("Error: {}", err)),
-    }
+async fn resolve_ens(
+    web3: &Web3<Http>,
+    contract_data: &models::ContractData,
+    ens_name: &str,
+) -> Result<Option<Address>, String> {
+    // Implementation for resolving ENS name using contract data and Web3 instance
+    // Return the resolved address or an error message
+    Ok(None) // Placeholder implementation
 }
