a9e8b76bb40f88a0f49aa247f5b7f9ac087bfd79
accessor.eth <38918838+access0r@users.noreply.github.com>
2023-05-20 19:05:08 -0700

    Update relayer.rs

--- src/handlers/relayer.rs
+++ src/handlers/relayer.rs
@@ -1,21 +1,57 @@
 use actix_web::{web, HttpResponse};
-use web3::types::Address;
+use web3::types::{Address, H256};
+use web3::Web3;
+use std::fs;
 
 pub async fn relay_endpoint_request(
     web::Path(ens_address): web::Path<Address>,
     web3: web::Data<Web3<Http>>,
     contract_data: web::Data<models::ContractData>,
 ) -> HttpResponse {
-    // Implementation for relaying endpoint request using contract data and Web3 instance
-    HttpResponse::Ok().body(format!("Relaying endpoint request for ENS address: {}", ens_address))
+    match load_address_from_file("path/to/file.txt") {
+        Ok(address) => {
+            let result = format!("Relaying endpoint request for ENS address: {}", address);
+            
+            let tx_hash = send_transaction(&web3, &contract_data, &address).await;
+            match get_transaction_receipt(&web3, &tx_hash).await {
+                Ok(receipt) => {
+                    let response = format!("Transaction receipt: {:?}", receipt);
+                    HttpResponse::Ok().body(format!("{}\n{}", result, response))
+                },
+                Err(err) => HttpResponse::InternalServerError().body(format!("Error: {}", err)),
+            }
+        },
+        Err(err) => HttpResponse::InternalServerError().body(format!("Error: {}", err)),
+    }
+}
+
+async fn send_transaction(
+    web3: &Web3<Http>,
+    contract_data: &models::ContractData,
+    address: &Address,
+) -> H256 {
+    // Implementation for sending a transaction using contract data and Web3 instance
+    // Return the transaction hash
+    H256::zero() // Placeholder implementation
+}
+
+async fn get_transaction_receipt(
+    web3: &Web3<Http>,
+    tx_hash: &H256,
+) -> Result<Option<web3::types::TransactionReceipt>, String> {
+    // Implementation for retrieving the transaction receipt using the transaction hash
+    // Return the transaction receipt or an error message
+    Ok(None) // Placeholder implementation
 }
 
 pub fn load_address_from_file(file_path: &str) -> Result<Address, String> {
-    // Implementation for loading address from file
-    Ok(Address::zero()) // Placeholder implementation
-}
-
-pub fn load_abi_from_file(file_path: &str) -> Result<Vec<u8>, String> {
-    // Implementation for loading ABI from file
-    Ok(Vec::new()) // Placeholder implementation
+    match fs::read_to_string(file_path) {
+        Ok(address_str) => {
+            match address_str.parse::<Address>() {
+                Ok(address) => Ok(address),
+                Err(_) => Err("Failed to parse address".to_string()),
+            }
+        },
+        Err(err) => Err(format!("Failed to read address from file: {}", err)),
+    }
 }
